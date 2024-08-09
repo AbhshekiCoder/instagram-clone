df6d36bef744b0cb03a26e6c5a9e4b1d0b2b8590
Abhishek Gour <117675628+AbhshekiCoder@users.noreply.github.com>
2024-08-09 15:42:53 +0530

    Update PrivateRoute.jsx

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 function PrivateRoute(){
     const Profile = localStorage.getItem("user_id");
-    return Profile?<Outlet/>:<Navigate to ="/SigninModal"/>
+    return Profile?<Navigate to ="/SigninModal"/>:<Outlet/>
 
 }
 export default PrivateRoute;
